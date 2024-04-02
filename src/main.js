5be37e7db79c968c9df6a7f30f507d387df97bd3
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-04-02 18:38:13 +0200

    pseudocode for development

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+// Create a Todo list app using js modules
+// Show the list of Todos if there are any stored in the localStorage
+// Show the projects if there are any as tabs like in restaurant app
+// The user should be able to navigate between the different todos projects and see only those that are related to the selected project (filter the other ones)
+// Add a button for adding the New Task
+// The button will open a modal
+// The modal should include a form for adding a task
+// The form should include fields: Title, Description, dueDate, Priority & the Project name where the task should be placed into (if there is aldready created some projects)
+// Add a form submit
+// When form is submitted, the task will be added to the localStorage
+// Then the task will appear in the list of todos
+// The task can be checked as Done, can be Edited or Removed (add buttons or icons to implement this functionality)
