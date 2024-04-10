8cb0164e8940aa9b0f90c1c22038ac6daf70b685
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-04-10 18:54:01 +0200

    added eslint

--- src/main.js
+++ src/main.js
@@ -3,17 +3,3 @@ import './style.css';
 import home from './modules/home';
 
 home();
-
-const body = document.querySelector('body');
-
-// Create a Todo list app using js modules
-// Show the list of Todos if there are any stored in the localStorage
-// Show the projects if there are any as tabs like in restaurant app
-// The user should be able to navigate between the different todos projects and see only those that are related to the selected project (filter the other ones)
-// Add a button for adding the New Task
-
-// Add a form submit
-// When form is submitted, the task will be added to the localStorage
-// Then the task will appear in the list of todos
-
-// The task can be checked as Done, can be Edited or Removed (add buttons or icons to implement this functionality)
